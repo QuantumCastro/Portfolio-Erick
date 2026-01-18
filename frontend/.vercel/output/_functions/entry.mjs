58075db7d860bf5bfea25b54e23d49647189c714
Erick <ejcmate1@gmail.com>
2026-01-18 00:14:50 -0600

    Quality gates

--- frontend/.vercel/output/_functions/entry.mjs
+++ frontend/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CBQmyzpN.mjs';
-import { manifest } from './manifest_CDzODZAk.mjs';
+import { manifest } from './manifest_L2bKAfF7.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -21,7 +21,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "0304b357-35be-4185-8890-c72fb3fc1c07",
+    "middlewareSecret": "18db1ade-0cc4-4fa8-b4e7-2984b3a642d5",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
